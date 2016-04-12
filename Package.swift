ef7f7694b3794dbc4dca299f1f88af78bfc4935a
noppoman <yuki@miketokyo.com>
2016-04-13 01:14:38 +0900

    lowercased

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ import PackageDescription
 let package = Package(
 	name: "Crypto",
 	dependencies: [
-      .Package(url: "https://github.com/Open-Swift/C7.git", majorVersion: 0, minor: 4),
+      .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 4),
       .Package(url: openSSLRepo, majorVersion: 0, minor: 1)
   ]
 )
