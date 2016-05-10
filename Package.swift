0167da23e7a362bae265b278eb834169720f2900
noppoman <yuki@miketokyo.com>
2016-05-11 02:39:38 +0900

    update dependencies

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ import PackageDescription
 let package = Package(
 	name: "Crypto",
 	dependencies: [
-      .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 6),
+      .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 7),
       .Package(url: openSSLRepo, majorVersion: 0, minor: 1)
   ]
 )
