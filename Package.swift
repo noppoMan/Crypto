c61d1179ac5f5f399768d6de13d94f98ead2b8e4
noppoman <yuki@miketokyo.com>
2016-06-19 16:07:42 +0900

    Bump C7 to 0.8

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "Crypto",
 	dependencies: [
-      .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 7),
+      .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 8),
       .Package(url: "https://github.com/noppoMan/CNPOpenSSL.git", majorVersion: 0, minor: 2)
   ]
 )
